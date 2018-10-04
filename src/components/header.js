0a77e2327ab2db8f9feda5192589fe2c8967406c
Alexander Rizhikoff <designcreative.me@gmail.com>
2018-10-04 21:16:52 +0300

    Added cards and black transparent header

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 import logo from '../images/logo-designcode.svg'
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img src={logo} width="30" /></Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy"><button>Buy</button></Link>
-    </div>
-  </div>
-)
+class Header extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src={logo} alt="Logo Design+Creative" width="30" /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/hire-me"><button>Hire Me</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
