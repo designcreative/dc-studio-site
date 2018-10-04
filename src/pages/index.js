a4c0e4b9d94225466d946ba2236b6f0f8c5ea0d9
Alexander Rizhikoff <designcreative.me@gmail.com>
2018-10-04 23:09:21 +0300

    Add Cell component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import Card from '../components/Card'
 import Section from '../components/Section'
 import Wave from '../components/Wave'
+import staticdata from '../../staticdata.json'
+import Cell from '../components/Cell'
+
+const SectionCaption = styled.p `
+  font-weight: 600;
+  font-size: 18px;
+  text-transform: uppercase;
+  color: #94a4ba;
+  text-align: center;
+`
+const SectionCellGroup = styled.div `
+  max-width: 800px;
+  margin: 0 auto 100px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 20px;
+  padding: 0 20px;
+
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`
 
 const IndexPage = () => (
   <Layout>
@@ -52,6 +75,15 @@ const IndexPage = () => (
         logo={require('../images/logo-react.png')}
         title="React for Developers"
         text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components. Grid CSS, animations, interactions, dynamic data with Content for and deploying your site with Netlify." />
+    
+    <SectionCaption>12 sections - 6 hours</SectionCaption>
+    <SectionCellGroup>
+      {staticdata.cells.map(cell => (
+        <Cell 
+          title={cell.title}
+          image={cell.image} />
+      ))}
+    </SectionCellGroup>
   </Layout>
 )
 
